6f40664ee5e06a1c01150607c4a899054ed39128
bass1st <bass1st@live.com>
2022-07-04 12:03:21 -0700

    Add basic authentication with JWT

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    // When creating extension methods, class needs to be "static" because we don't want to create another instances of already existing class
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // AddScoped() method keeps the service alive until http request is completed. Exactly what's needed for creating a JWT token
+            // AddSingleton() keeps the service alive through the lifespan of an app, which is not appropriate for this task
+            // AddTransient() keeps the service alive only up until the method execution is finished, which is not ideal for http requests
+            services.AddScoped<ITokenService, TokenService>();
+            // Lambda expression (passing an expression as a parameter)
+            // Add db connection string from appsettings.Development.json
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
