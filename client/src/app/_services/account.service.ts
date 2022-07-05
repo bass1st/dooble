1c7af7329aa64bc790eeeede06b47b075f66877d
bass1st <bass1st@live.com>
2022-07-05 13:12:24 -0700

    Add navigation, home and register in client app

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,54 @@
+// Unlike components, a service is a singleton - the data that it stores is available through the lifetime of an application
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl: string = 'https://localhost:5001/api/';
+  // Create an observable to store an user
+  // ReplaySubject is kind of an "object buffer" observable, we define how many recent value(s) of an object we want to store
+  // In this case, the size of a buffer is (1), hence one and only value is stored
+  private currentUserSource = new ReplaySubject<User>(1);
+  // For observable type variables, convention is to append the variable name with the "$" sign
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+}
